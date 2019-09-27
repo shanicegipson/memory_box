7d72b356d41c1ff64bcf6745cdbd538c886f6244
Shanice Gipson <shanicegipson13@gmail.com>
2019-09-27 17:28:43 -0500

    update

--- src/components/pages/GuestPage/GuestPage.js
+++ src/components/pages/GuestPage/GuestPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../../redux/mapStoreToProps';
-import MediaItem from '../../../MediaItem/MediaItem';
+import MediaItem from '../../../components/MediaItem/MediaItem';
 
 import { withRouter } from 'react-router-dom';
 
